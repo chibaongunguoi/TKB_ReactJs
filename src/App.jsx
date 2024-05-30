2bea8d1044f8c46e0419fa889fc76a021d9886f1
chibaongunguoi <chibaongunguoi@gmail.com>
2024-05-30 20:07:22 +0700

    a

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import Form from './Form'
-
+import { Analytics } from "@vercel/analytics/react"
 
 
 export default function App() {
@@ -8,8 +8,10 @@ export default function App() {
   let [isSubmit,setIsSubmit]=useState(JSON.parse(localStorage.getItem('subjects'))!=null);
   return(
   <div id="wrapper">
+    <Analytics/>
      {!isSubmit ?
       (<form id="row_number" onSubmit={()=>setIsSubmit(true)}>
+        
         <label >Nhập số môn bạn học trong học kì này (từ 1 đến 20):</label>
         <input  id="row_n" value={count}
          onChange={(e) => setCount(e.target.value)}/>
