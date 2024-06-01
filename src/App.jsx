c08018ddd1a86973db85a62c8ad9d4620256e2e7
chibaongunguoi <chibaongunguoi@gmail.com>
2024-06-01 17:40:28 +0700

    a

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,39 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Form from './Form'
 import { Analytics } from "@vercel/analytics/react"
 import {initSubject} from './render_table'
 
+let idForm=1;
 export default function App() {
   let [count,setCount]=useState('');
   let [currentForm,setCurrentForm]=useState(localStorage.getItem('currentForm')==null?1:localStorage.getItem('currentForm'));
   let isSubmit=(JSON.parse(localStorage.getItem(`subject${currentForm}`))!=null);
   let [countForm,setCountForm]=useState(localStorage.getItem('countForm')==null?1:localStorage.getItem('countForm'));
-  console.log(countForm,'render');
-  function addForm(){
-    setCountForm(parseInt(countForm)+1);
-    console.log(parseInt(countForm)+1,'after setcountForm')
-    localStorage.setItem('countForm',parseInt(countForm)+1);
-  }
+  let [formList,setFormList]=useState(()=>{
+    if (JSON.parse(localStorage.getItem('formList'))&&JSON.parse(localStorage.getItem('formList')).length>0)
+      {
+        idForm=JSON.parse(localStorage.getItem('formList'))[JSON.parse(localStorage.getItem('formList')).length-1].id;
+        idForm++;
+      return JSON.parse(localStorage.getItem('formList'));
+      }
+    
+    return [{id:idForm++,title:'TKB 1'}];
+  })
+  useEffect(()=>{
+    localStorage.setItem('currentForm',currentForm );
+  },[currentForm])
+  useEffect(()=>{
+    if (formList.length==0) setFormList([{id:idForm++,title:'TKB 1'}]);
+    localStorage.setItem('formList', JSON.stringify(formList));
+  },[formList])
   function changecurrentForm(e){
     setCurrentForm(e.target.value);
-    localStorage.setItem('currentForm',e.target.value);
   }
+  console.log(countForm,'render');
   return(
   <div id="wrapper">
     <Analytics />
-    <Bar count={countForm} addForm={addForm} changecurrentForm={changecurrentForm} currentForm={currentForm}/> 
+    <Bar setCurrentForm={setCurrentForm}setFormList={setFormList}changecurrentForm={changecurrentForm} currentForm={currentForm} formList={formList}/> 
      {!isSubmit ?
       (<form id="row_number" onSubmit={()=>localStorage.setItem(`subject${currentForm}`,JSON.stringify( initSubject(count)))}>
         <label >Nhập số môn bạn học trong học kì này (từ 1 đến 20):</label>
@@ -35,25 +47,52 @@ export default function App() {
     </div>
   )
 }
-function Bar({count,addForm,changecurrentForm,currentForm}){
+function Bar({setCurrentForm,setFormList,editform,delform,addForm,changecurrentForm,currentForm,formList}){
+  
+  function addForm(){
+    let title=prompt('TKB mới này sẽ có tên là:');
+    setFormList([...formList,{id:idForm++,title:title}]);
+  }
+  function delform(e){
+    console.log(e.target.value,formList)
+    setFormList(formList.filter((form => form.id!=e.target.value)))
+    localStorage.removeItem(`subject${e.target.value}`);
+    if (e.target.value==currentForm){
+      setCurrentForm(formList[0].id);
+    }
+    // let bar = confirm('Confirm or deny');
+  }
+  function editform(e){
+    console.log(e.target,formList)
+    let title=prompt('Bạn muốn đổi tên thành:');
+    setFormList(formList.map((form => {
+      if (form.id!=e.target.value){
+        return form; 
+      }
+      else return {...form,title:title}
+    })))
+    // let bar = confirm('Confirm or deny');
+  }
+  
   let tkb=[];
   let style={margin:0};
-  for (let i=1;i<=count;i++){
-    if (i==currentForm)
+  
+  for (let i of formList){
+    if (i.id==currentForm)
     tkb.push(
     <>
     <section class="nav-item nav-link active" style={{backgroundColor:'blue'}}>
-    <button style={style} data-toggle="tab" onClick={(e)=>{changecurrentForm(e)}}class=" active" value={i}>TKB {i}</button>
-    <button style={style} class="btn btn-success btn-sm rounded-0 text-white" type="button" data-toggle="tooltip" data-placement="top" title="Edit"><i class="fa fa-edit"></i></button>
-    <button style={style} onclick={()=> confirm('Bạn có chắc chắn xóa bản ghi này?')} class="btn btn-danger btn-sm rounded-0 text-white" type="button" data-toggle="tooltip" data-placement="top" title="Delete"><i class="fa fa-trash"></i></button>
+    <button style={style} data-toggle="tab" onClick={(e)=>{changecurrentForm(e)}}class=" active" value={i.id}>{i.title}</button>
+    <button style={style} value={i.id} onClick={editform}class="btn btn-success btn-sm rounded-0 text-white"  data-toggle="tooltip" data-placement="top" title="Edit">ĐỔI TÊN</button>
+    <button style={style} value={i.id} onClick={delform} class="btn btn-danger btn-sm rounded-0 text-white"  data-toggle="tooltip" data-placement="top" title="Delete">XÓA</button>
     </section>
     </>)
     else
     tkb.push(<>
       <section class="nav-item nav-link">
-      <button style={style} data-toggle="tab" onClick={(e)=>{changecurrentForm(e)}} value={i}>TKB {i}</button>
-      <button style={style} class=" btn btn-success btn-sm rounded-0 text-white" type="button" data-toggle="tooltip" data-placement="top" title="Edit"><i class="fa fa-edit"></i></button>
-      <button style={style} onclick="return confirm('Bạn có chắc chắn xóa bản ghi này?')" class="btn btn-danger btn-sm rounded-0 text-white" type="button" data-toggle="tooltip" data-placement="top" title="Delete"><i class="fa fa-trash"></i></button>
+      <button style={style} data-toggle="tab" onClick={(e)=>{changecurrentForm(e)}} value={i.id}>{i.title}</button>
+      <button style={style}value={i.id} onClick={editform} class=" btn btn-success btn-sm rounded-0 text-white" type="button" data-toggle="tooltip" data-placement="top" title="Edit">ĐỔI TÊN</button>
+      <button style={style} value={i.id} onClick={delform} class="btn btn-danger btn-sm rounded-0 text-white" type="button" data-toggle="tooltip" data-placement="top" title="Delete">XÓA</button>
       </section>
       </>)
   }
