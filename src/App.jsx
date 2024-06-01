bc162d86971eb24d79d7c9cc39e521299251bfb5
chibaongunguoi <chibaongunguoi@gmail.com>
2024-06-01 20:30:11 +0700

    a

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,9 @@ export default function App() {
     localStorage.setItem('currentForm',currentForm );
   },[currentForm])
   function changecurrentForm(e){
-    setCurrentForm(e.target.value);
+    e.preventDefault();
+    console.log(e.target.dataset.value)
+    setCurrentForm(e.target.dataset.value);
     // e.target.style.backgroundColor='blue';
   }
   return(
@@ -81,8 +83,8 @@ function Bar({setCurrentForm,setFormList,editform,delform,addForm,changecurrentF
   
   for (let i of formList){
     tkb.push(<>
-      <section class="nav-item nav-link" style={i.id==currentForm?{backgroundColor:'blue'}:null}>
-      <button style={style} data-toggle="tab" onClick={(e)=>{changecurrentForm(e)}} value={i.id}>{i.title}</button>
+      <section class="nav-item nav-link" >
+      <a class={i.id==currentForm?"":'text-muted'} href='' style={style} data-toggle="tab" onClick={(e)=>{changecurrentForm(e)}} data-value={i.id}>{i.title}</a>
       <button style={style}value={i.id} onClick={editform} class=" btn btn-success btn-sm rounded-0 text-white" type="button" data-toggle="tooltip" data-placement="top" title="Edit">ĐỔI TÊN</button>
       <button hidden={i.id==formList[0].id? true:false}style={style} value={i.id} onClick={delform} class="btn btn-danger btn-sm rounded-0 text-white" type="button" data-toggle="tooltip" data-placement="top" title="Delete">XÓA</button>
       </section>
