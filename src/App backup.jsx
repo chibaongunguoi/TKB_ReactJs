201c89a84b052c20ff07eb608c38bf17e498c3c7
chibaongunguoi <chibaongunguoi@gmail.com>
2024-06-01 18:51:02 +0700

    a

--- src/App backup.jsx
+++ src/App backup.jsx
@@ -16,16 +16,18 @@ export default function App() {
         idForm++;
       return JSON.parse(localStorage.getItem('formList'));
       }
-    // console.log(idForm);
+    
     return [{id:idForm++,title:'TKB của tôi'}];
   })
   useEffect(()=>{
     localStorage.setItem('currentForm',currentForm );
   },[currentForm])
+  
   function changecurrentForm(e){
     setCurrentForm(e.target.value);
     // e.target.style.backgroundColor='blue';
   }
+  console.log(countForm,'render');
   return(
   <div id="wrapper">
     <Analytics />
@@ -79,11 +81,21 @@ function Bar({setCurrentForm,setFormList,editform,delform,addForm,changecurrentF
   let style={margin:0};
   
   for (let i of formList){
+    if (i.id==currentForm)
+    tkb.push(
+    <>
+    <section class="nav-item nav-link active" style={{backgroundColor:'blue'}}>
+    <button style={style} data-toggle="tab" onClick={(e)=>{changecurrentForm(e)}}class=" active" value={i.id}>{i.title}</button>
+    <button style={style} value={i.id} onClick={editform}class="btn btn-success btn-sm rounded-0 text-white"  data-toggle="tooltip" data-placement="top" title="Edit">ĐỔI TÊN</button>
+    <button style={style} value={i.id} onClick={delform} class="btn btn-danger btn-sm rounded-0 text-white"  data-toggle="tooltip" data-placement="top" title="Delete">XÓA</button>
+    </section>
+    </>)
+    else
     tkb.push(<>
-      <section class="nav-item nav-link" style={i.id==currentForm?{backgroundColor:'blue'}:null}>
+      <section class="nav-item nav-link">
       <button style={style} data-toggle="tab" onClick={(e)=>{changecurrentForm(e)}} value={i.id}>{i.title}</button>
       <button style={style}value={i.id} onClick={editform} class=" btn btn-success btn-sm rounded-0 text-white" type="button" data-toggle="tooltip" data-placement="top" title="Edit">ĐỔI TÊN</button>
-      <button hidden={i.id==formList[0].id? true:false}style={style} value={i.id} onClick={delform} class="btn btn-danger btn-sm rounded-0 text-white" type="button" data-toggle="tooltip" data-placement="top" title="Delete">XÓA</button>
+      <button style={style} value={i.id} onClick={delform} class="btn btn-danger btn-sm rounded-0 text-white" type="button" data-toggle="tooltip" data-placement="top" title="Delete">XÓA</button>
       </section>
       </>)
   }
